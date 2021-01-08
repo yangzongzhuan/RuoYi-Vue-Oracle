ba46498ca7c897ede36284cbde8d605ed57a768d
RuoYi <yzz_ivy@163.com>
2021-01-08 11:06:01 +0800

    代码生成模板支持主子表

--- src/main/java/com/ruoyi/project/tool/gen/domain/GenTable.java
+++ src/main/java/com/ruoyi/project/tool/gen/domain/GenTable.java
@@ -28,11 +28,17 @@ public class GenTable extends BaseEntity
     @NotBlank(message = "表描述不能为空")
     private String tableComment;
 
+    /** 关联父表的表名 */
+    private String subTableName;
+
+    /** 本表关联父表的外键名 */
+    private String subTableFkName;
+
     /** 实体类名称(首字母大写) */
     @NotBlank(message = "实体类名称不能为空")
     private String className;
 
-    /** 使用的模板（crud单表操作 tree树表操作） */
+    /** 使用的模板（crud单表操作 tree树表操作 sub主子表操作） */
     private String tplCategory;
 
     /** 生成包路径 */
@@ -63,6 +69,9 @@ public class GenTable extends BaseEntity
 
     /** 主键信息 */
     private GenTableColumn pkColumn;
+
+    /** 子表信息 */
+    private GenTable subTable;
 
     /** 表列信息 */
     @Valid
@@ -117,6 +126,26 @@ public class GenTable extends BaseEntity
     public void setTableComment(String tableComment)
     {
         this.tableComment = tableComment;
+    }
+
+    public String getSubTableName()
+    {
+        return subTableName;
+    }
+
+    public void setSubTableName(String subTableName)
+    {
+        this.subTableName = subTableName;
+    }
+
+    public String getSubTableFkName()
+    {
+        return subTableFkName;
+    }
+
+    public void setSubTableFkName(String subTableFkName)
+    {
+        this.subTableFkName = subTableFkName;
     }
 
     public String getClassName()
@@ -219,6 +248,16 @@ public class GenTable extends BaseEntity
         this.pkColumn = pkColumn;
     }
 
+    public GenTable getSubTable()
+    {
+        return subTable;
+    }
+
+    public void setSubTable(GenTable subTable)
+    {
+        this.subTable = subTable;
+    }
+
     public List<GenTableColumn> getColumns()
     {
         return columns;
@@ -287,6 +326,16 @@ public class GenTable extends BaseEntity
     public void setParentMenuName(String parentMenuName)
     {
         this.parentMenuName = parentMenuName;
+    }
+
+    public boolean isSub()
+    {
+        return isSub(this.tplCategory);
+    }
+
+    public static boolean isSub(String tplCategory)
+    {
+        return tplCategory != null && StringUtils.equals(GenConstants.TPL_SUB, tplCategory);
     }
 
     public boolean isTree()
