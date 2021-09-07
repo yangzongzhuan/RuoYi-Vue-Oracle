2a24c04f0fddf9f63468ab7c018525007ec5d5a1
kwinvip <kwin_h@163.com>
2021-09-07 08:39:09 +0800

    Update DataScopeAspect.java
    数据权限查询异常修复

--- src/main/java/com/ruoyi/framework/aspectj/DataScopeAspect.java
+++ src/main/java/com/ruoyi/framework/aspectj/DataScopeAspect.java
@@ -122,7 +122,7 @@ public class DataScopeAspect
             else if (DATA_SCOPE_DEPT_AND_CHILD.equals(dataScope))
             {
                 sqlString.append(StringUtils.format(
-                        " OR {}.dept_id IN ( SELECT dept_id FROM sys_dept WHERE dept_id = {} or find_in_set( {} , ancestors ) )",
+                        " OR {}.dept_id IN ( SELECT dept_id FROM sys_dept WHERE dept_id = {} or find_in_set( {} , ancestors ) > 0 )",
                         deptAlias, user.getDeptId(), user.getDeptId()));
             }
             else if (DATA_SCOPE_SELF.equals(dataScope))
